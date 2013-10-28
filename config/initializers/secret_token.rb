7988f63109dbc90f90d8e40598bb579a8f9f7a30
zander <aamagtipon@voyagerinnovation.com>
2013-10-28 22:42:16 +0800

    Initial project setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GiftLauncher::Application.config.secret_key_base = '1b7c47f0a7471f836913c701c628ac6b488dff80a174e033c1e112c32144c8755e286a4224b9a5d338acb4c4e562c997231182e2cbbdf6f8594069e9095759dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GiftLauncher::Application.config.session_store :cookie_store, key: '_gift_launcher_session'
