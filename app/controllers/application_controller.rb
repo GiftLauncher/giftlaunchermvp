775626d3db814dcfeb7e3e3b7351317c9e5a5fc5
zander <aamagtipon@voyagerinnovation.com>
2013-11-03 11:26:38 +0800

    user authentication module via fb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+  include Authentication
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from FbGraph::Exception, :with => :fb_graph_exception
+
+  def fb_graph_exception(e)
+    flash[:error] = {
+        :title   => e.class,
+        :message => e.message
+    }
+    redirect_to root_url
+  end
+
 end
--- app/models/facebook.rb
+++ app/models/facebook.rb
@@ -1,13 +1,25 @@
 class Facebook < ActiveRecord::Base
   belongs_to :user
 
+  def profile
+    @profile ||= FbGraph::User.me(self.access_token).fetch
+  end
+
   class << self
     def config
       GiftLauncher::Application.config
     end
 
     def auth(redirect_uri = nil)
-      FbGraph::Auth.new config.fb_app_id, config.fb_app_secret, :redirect_uri => redirect_uri
+      FbGraph::Auth.new(config.fb_app_id, config.fb_app_secret, :redirect_uri => redirect_uri)
+    end
+
+    def identify(fb_user)
+      fb = find_or_initialize_by(:identifier => fb_user.identifier.try(:to_s))
+      access_token = fb_user.access_token.access_token
+      fb.access_token = access_token if fb.access_token != access_token
+      fb.save! if fb.persisted? && fb.access_token_changed?
+      fb
     end
   end
 
