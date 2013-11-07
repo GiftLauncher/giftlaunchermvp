181c678fc962d5e57f54ee353ae2945eb410eaea
zander <aamagtipon@voyagerinnovation.com>
2013-11-07 16:52:47 +0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131107072800) do
+ActiveRecord::Schema.define(version: 20131107075002) do
+
+  create_table "events", force: true do |t|
+    t.string   "name"
+    t.string   "description", limit: 1000
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "facebooks", force: true do |t|
     t.integer  "user_id"
